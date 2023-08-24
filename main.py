96deecc6df814daf5594088cff04193d56bb185a
Jackson-Vitorio <136825439+Jackson-Vitorio@users.noreply.github.com>
2023-08-24 09:16:58 -0300

    finalização e polimento

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
-
 import json
 import requests
-import translators as ts
+from translate import Translator
 
-
+s = Translator(from_lang="en", to_lang="pt")
 def magias():
-    spell = input("digite o nome da magia: ")
+    spell = input("Digite o nome da magia(usando "-" como espaço): ")
     req = requests.get("https://www.dnd5eapi.co/api/spells/" + spell)
     saida = json.loads(req.text)
-    print("\n Nome: ", saida["name"], "\n")
-    print("Descrição: ", saida["desc"], "\n")
+    print("\nNome: ", saida["name"], "\n")
+    desc = saida["desc"][0]
+    res = s.translate(desc)
+    print("Descrição: ", res, "\n")
     print("Tempo de cast: ", saida["casting_time"], "\n")
-    print("Componentes: ", saida["components"], "\n")
+
+    if "M" in saida["components"]:
+        material_desc = saida["material"]
+        material_traduct = s.translate(material_desc)
+        print("Componente M (Material):", material_traduct, "\n")
+
     return
-
-
 
 
 def monstros():
-    monster = input("digite o nome do monstro: ")
+    monster = input("Digite o nome do monstro: ")
     req = requests.get("https://www.dnd5eapi.co/api/monsters/" + monster)
     saida = json.loads(req.text)
-    print("\n Nome: ", saida["name"], "\n")
-    print("Tamanho: ", saida["size"], "\n")
-    print("Tipo: ", saida["type"], "\n")
+
+    print("\nNome: ", s.translate(saida["name"]), "\n")
+    print("Tamanho: ", s.translate(saida["size"]), "\n")
+    print("Tipo: ", s.translate(saida["type"]), "\n")
     print("Armadura: ", saida["armor_class"], "\n")
     print("Pontos de vida: ", saida["hit_points"], "\n")
-    print("Ações: ", saida["actions"])
+
+    if "actions" in saida:
+        translated_actions = [s.translate(action["name"]) for action in saida["actions"]]
+        print("Ações: ", ", ".join(translated_actions))
+
     return
 
 
-
-
 escolhas = int(input(
-    "oque voce deseja procurar ? \n para magias use 1 \n para monstros use 2: "))
+    "O que você deseja procurar?\nPara magias, use 1\nPara monstros, use 2: "))
 if escolhas == 1:
     magias()
 elif escolhas == 2:
     monstros()
 else:
-    print("erro")
-
+    print("Erro: Opção inválida.")
